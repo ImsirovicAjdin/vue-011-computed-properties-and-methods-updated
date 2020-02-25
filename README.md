2f6dfa0e1a9d9b1026208d59915c40423798df3f
FlowOverstack <ajdika@live.com>
2020-02-25 21:13:14 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-011-computed-properties-and-methods-updated
-Created with CodeSandbox
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <p>
+      Enter owner name:
+      <input v-model.lazy="ownerName" placeholder="enter owner">
+    </p>
+    <p>
+      Enter thing owned:
+      <input v-model.lazy="thing" placeholder="enter thing">
+    </p>
+    <h1 v-if="ownerName && thing">{{ ownerHasThing }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      ownerName: "",
+      thing: ""
+    };
+  },
+  computed: {
+    // a computed getter
+    ownerHasThing: function() {
+      // `this` points to the Vue instance's data option
+      return this.ownerName + " has a " + this.thing;
+    }
+  }
+};
+</script>
+
